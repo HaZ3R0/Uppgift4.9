1dbac8700f9f5421f16062d7fd1feb48663d4eef
HaZ3R0 <megamackan@outlook.com>
2023-10-24 19:14:33 +0200

    Add project files.

---
+++ Uppgift4.9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Uppgift_4._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur stor bas vill du ha på din triangel?");
+            string bastext = Console.ReadLine();
+            int bas = int.Parse(bastext);
+            for(int i = 1; i <= bas; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        Console.ReadKey();
+        }
+    }
+}
